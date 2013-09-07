a0f7407044db7d09739f2543633e9b4263fd303b
bbugguj <bbugguj@gmail.com>
2013-09-07 14:43:38 +0900

    aws 정보 입력

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV['S3_BUCKET_NAME']
   config.aws_acl    = :public_read
-  config.asset_host = 'http://rorlab.s3-website-ap-northeast-1.amazonaws.com'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/vsns'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
   config.aws_credentials = {
